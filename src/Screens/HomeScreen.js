824f3fbb4a8e9dff311361b81cbf1e7cdebe916e
kingdeej <devonhenry10196@gmail.com>
2021-11-08 10:32:49 -0600

    first commit

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -4,14 +4,12 @@ import Footer from "../components/Footer";
 import GetStartedFloating from "../components/GetStartedFloating";
 import Hero from "../components/Hero";
 import InfoPage from "../components/InfoPage";
-import OurLatestWork from "../components/OurLatestWork";
 import Reviews from '../components/Reviews'
 
 export default function HomeScreen() {
     return (
     <div>
         <Hero />
-        <OurLatestWork />
         <InfoPage />
         <Reviews />
         <AboutUs />
