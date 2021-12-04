711ec01daec38f10b466d8c1b693b4a818a605fb
kingdeej <devonhenry10196@gmail.com>
2021-12-03 21:15:32 -0600

    first commit

--- src/Screens/ThankYou.js
+++ src/Screens/ThankYou.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom'
 export class ThankYou extends Component {
     render() {
         return (
-            <div className="pi-container pi-spacing">
+            <div className="thank-you-cont">
                 <div className="dtd-carpets">
-                    <h1 className="thanks">Thank You</h1>
+                    <h1 className="thanks">Thank You
+                    <hr /></h1>
+                    
                     <Link to="/" ><button className="thanks-btn">Home</button> </Link>              
                 </div>
             </div>
